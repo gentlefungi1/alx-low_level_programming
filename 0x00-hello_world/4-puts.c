7f63ff1a0e9484febbed4aace7cf2c8eb3dc466a
Kelvin <getokereafor@gmail.com>
2023-02-16 11:58:58 -0800

    commit new

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
-#include <stdio>
+#include <stdio.h>
 
  /**
-  * main - Entry point
-  * Return: Array 0 (Success)
+  * main - A program that prints a line with puts function
+  * Description: This prints Programming is a building
+  * Return: 0 (Success)
   */
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");			return (0);
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
 }
