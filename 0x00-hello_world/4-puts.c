a053d7cef6d7e679ca7007a57c3107450cf4101e
Kelvin <getokereafor@gmail.com>
2023-02-16 10:46:01 -0800

    hello printf

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio>
+
+ /**
+  * main - Entry point
+  * Return: Array 0 (Success)
+  */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");			return (0);
+}
