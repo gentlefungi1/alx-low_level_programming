1f8c98f8e9d843413e0f476821cd730b95e13c73
Kelvin <getokereafor@gmail.com>
2023-02-16 18:06:06 -0800

    new line

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
- /**
-  * main - A program that prints a line with puts function
-  * Description: This prints Programming is a building
-  * Return: 0 (Success)
-  */
+/**
+ * main - A program that prints a line with puts function
+ * Description: A program that prints a line with puts function
+ * Return: 0 (Success)
+ */
 
 int main(void)
 {
