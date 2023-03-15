59096069b7e8d3a17c0169c11dfcf7fdf2b517c9
Kelvin <getokereafor@gmail.com>
2023-03-15 14:06:32 -0700

    A library is not a luxury but one of the necessities of life

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,7 +1,7 @@
-#ifndef MAIN_H
-#define MAIN_H
-#include <stddef.h>
+#ifndef FILE_MAIN
+#define FILE_MAIN
 
+/*a list that was given from the task 0 */
 
 int _putchar(char c);
 int _islower(int c);
@@ -23,6 +23,5 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-
 
 #endif
